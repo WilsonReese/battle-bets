d20aae3b0d3cd04c32969e4626a73c491eed2f70
WilsonReese <71947861+WilsonReese@users.noreply.github.com>
2024-06-05 14:47:25 +0000

    got app page cleaned up

---
+++ App.style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const s = StyleSheet.create({
+    app: {
+        flex: 1,
+        backgroundColor: "white",
+        justifyContent: "space-evenly",
+        padding: 16,
+    },
+});
